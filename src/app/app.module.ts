1967edde11ee6f79cd6e3a180eed8fa232884463
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-05 23:18:57 +0700

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
