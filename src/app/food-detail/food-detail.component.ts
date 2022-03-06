f7815f4a6d9c20028b99df6a5b8e086589e3aa0d
VinhKyIT <vinhkyit2905@gmail.com>
2022-03-07 00:01:22 +0700

    add to cart service

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../services/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 
@@ -12,7 +13,7 @@ export class FoodDetailComponent implements OnInit {
 
   food!: Foods | undefined;
 
-  constructor(private activatedRoute: ActivatedRoute, private fs: FoodService) {
+  constructor(private activatedRoute: ActivatedRoute, private fs: FoodService, private cartService: CartService) {
     activatedRoute.params.subscribe(params => {
       if (params['id']) {
         this.food = this.fs.getById(params['id']);
@@ -21,6 +22,10 @@ export class FoodDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  addToCart(): void {
+    this.cartService.addToCart(this.food!);
   }
 
 }
