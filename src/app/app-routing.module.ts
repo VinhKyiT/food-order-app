b0385dcd630e76c6f078c44d15b869a34d9c4ecd
vinhkyit <vinhkyit2905@gmail.com>
2022-03-06 15:38:30 +0700

    add search function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: '/search/:searchItem', component: HomeComponent}
+  {path: 'search/:searchItem', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Foods } from './../shared/models/food';
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,9 +10,15 @@ import { FoodService } from '../services/food/food.service';
 export class HomeComponent implements OnInit {
   foods:Foods[] = [];
 
-  constructor(private fs:FoodService) { }
+  constructor(private fs:FoodService, private router:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods = this.fs.getAll();
+    this.router.params.subscribe(params => {
+      if (params['searchItem']) {
+        this.foods = this.fs.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+      } else {
+        this.foods = this.fs.getAll();
+      }
+    });
   }
 }
